356107d001c57a7c88032397af1ba39eb246d53d
PhoenixSmith <phoenix.smith.bcbsnc@gmail.com>
2018-09-04 20:06:11 -0400

    Added/updated JS to files

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
-  sub_btn();
+ sub_btn();
 
 });
 
 function sub_btn(){
   $('#sub_form').on('ajax:success', function (e){
-    window.location.reload()
-
+    e.preventDefault()
+    console.log('Whaaaaattttt?!?!')
   })
   
 }
